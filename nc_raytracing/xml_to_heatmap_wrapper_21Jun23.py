1076343c4328349e43fbc4c7974081ff5d179a2d
Zeyu Li <lizeyu090312@gmail.com>
2023-06-22 16:28:01 -0400

    added (not working yet) code for running a function instead of another file

--- nc_raytracing/xml_to_heatmap_wrapper_21Jun23.py
+++ nc_raytracing/xml_to_heatmap_wrapper_21Jun23.py
@@ -2,30 +2,215 @@ import subprocess
 import concurrent
 from concurrent.futures import wait
 import os
+import time
 
-BASE_PATH_BLENDER = '/res/'
-BASE_PATH_SIONNA = '/Sionna_coverage_maps/coverage_maps_new/'
+from PIL import Image
+import tensorflow as tf
+import numpy as np
+
+# Import Sionna RT components
+from sionna.rt import load_scene, Transmitter, Receiver, PlanarArray, Camera, Paths2CIR
+
+BASE_PATH_BLENDER = 'res/'
+BASE_PATH_SIONNA = 'Sionna_coverage_maps/coverage_maps_new/'
 # BLENDER_PATH should be the path I built, since the things are enabled
 # BLENDER_PATH = "/Users/zeyuli/blender-git/build_darwin/bin/Blender.app/Contents/MacOS/Blender"
 # START_FROM_IDX = 512
-NUM_OF_PROCESS = 8
+NUM_OF_PROCESS = 1
 EXTRA_HEIGHT = 2
 
+
+def sionna_run(height_file, extra_height, cm_cell_size, BASE_PATH_BLENDER, BASE_PATH_SIONNA, outer_idx):
+    """
+        Base path for the Blender files. The height files, pngs, and xml folders should be in BASE_PATH.
+        Organisation: BASE_PATH/Bl_terrain_img/, BASE_PATH/Bl_xml_files/, BASE_PATH/height_at_origin/,
+    """
+    """
+        Don't Parse arguments
+    """
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-t', '--height_file', type=str, required=True)
+    # # extra_height is added to the height above the xy-plane of the terrain/building at the origin when
+    # # placing Tx and when calculating signal strength in the coverage_map function
+    # parser.add_argument('-e', '--extra_height', type=float, required=True)
+    # # parser.add_argument('end', nargs='?', type=int, required=True)
+    # parser.add_argument('-c', '--cm_cell_size', type=float, required=False, default=10)
+    # parser.add_argument('-b', '--BASE_PATH_BLENDER', type=str, required=False, default='/res/')
+    # parser.add_argument('-s', '--BASE_PATH_SIONNA', type=str, required=True)
+    # parser.add_argument('-n', '--outer_idx', type=int, required=True)
+    # args = parser.parse_args()
+    # # print('height file name: ', args.height_file)
+    # # print('extra height: ', args.extra_height)
+    BASE_PATH = BASE_PATH_BLENDER
+    BASE_PATH_SIONNA = BASE_PATH_SIONNA
+    f_ptr_H = open(BASE_PATH + 'height_at_origin/' + height_file, 'r')
+
+    """
+        Setting up the environment, including the GPUs
+    """
+    gpu_num = 0  # Use "" to use the CPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{gpu_num}"
+
+    gpus = tf.config.list_physical_devices('GPU')
+    print(gpus)
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), 'Physical GPUs, ', len(logical_gpus), 'Logical GPUs')
+        except RuntimeError as e:
+            pass
+            # print(e)  # Avoid warnings from TensorFlow
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+    tf.random.set_seed(1)  # Set global random seed for reproducibility
+
+    def get_data_from_HeightFile(f_ptr_height):
+        try:
+            lines = f_ptr_height.readlines()
+            line = lines[0]
+            line = line.replace('(', '')
+            line = line.replace(')', '')
+            line = line.replace('\n', '')
+            line = line.split(',')
+            f_name = line[-1]
+            line = line[0:-1]  # disregard FileName
+            # file format: (minLon, maxLat, maxLon, minLat), building_to_area ratio, height, name
+            minLonOut, maxLatOut, maxLonOut, minLatOut, percent, height = [float(l) for l in line]
+            return minLonOut, maxLatOut, maxLonOut, minLatOut, percent, height, f_name
+        except Exception as e:
+            raise e
+
+    def cm_routine(extra_height, f_ptr_height):
+        try:
+            _, _, _, _, _, height_at_origin, file_name = get_data_from_HeightFile(f_ptr_height=f_ptr_height)
+            start_loc = time.time()
+            scene = load_scene(BASE_PATH + 'Bl_xml_files/' + file_name + '/' + file_name + '.xml')
+            # print('load scene time: ', str(time.time() - start_loc))
+            scene.tx_array = PlanarArray(num_rows=1,
+                                         num_cols=1,
+                                         vertical_spacing=0.5,
+                                         horizontal_spacing=0.5,
+                                         pattern="tr38901",
+                                         polarization="VH")
+
+            # Configure antenna array for all receivers
+            scene.rx_array = PlanarArray(num_rows=1,
+                                         num_cols=1,
+                                         vertical_spacing=0.5,
+                                         horizontal_spacing=0.5,
+                                         pattern="iso",
+                                         polarization="cross")
+            # Add a transmitter
+            tx = Transmitter(name="tx",
+                             position=[0, 0, height_at_origin + extra_height],
+                             orientation=[0, 0, 0])
+            scene.add(tx)
+
+            scene.frequency = 2.14e9  # in Hz; implicitly updates RadioMaterials
+            scene.synthetic_array = True  # If set to False, ray tracing will be done per antenna element (slower for large arrays)
+
+            # Compute coverage map
+            start_loc = time.time()
+            cm = scene.coverage_map(max_depth=8, cm_center=[0, 0, height_at_origin + extra_height],
+                                    cm_orientation=[0, 0, 0],
+                                    cm_cell_size=[cm_cell_size, cm_cell_size], cm_size=[1000, 1000])
+            # print('compute cm time: ', str(time.time() - start_loc))
+            # Visualize coverage in preview
+            # scene.preview(coverage_map=cm, resolution=[1000, 600])
+            return cm, scene, file_name
+        except Exception as e:
+            raise e
+
+    def save_routine(cm, img_path):
+        try:
+            cm_tensor = cm.as_tensor()
+            cm_2D = cm_tensor.numpy()[0, :, :]
+            cm_2D[cm_2D == 0] = np.nan
+            cm_2D_dB = 10 * np.log10(cm_2D)
+            cm_img = Image.fromarray(cm_2D_dB)
+            cm_img.save(img_path)
+            return
+        except Exception as e:
+            raise e
+
+    def run_routine():
+        try:
+            print('Calculating for index:', str(idx))
+            coverage_map, scene, fName = cm_routine(extra_height=extra_height, f_ptr_height=f_ptr_H)
+            print(fName)
+            image_path = BASE_PATH_SIONNA + fName + '.tiff'
+            save_routine(coverage_map, image_path)
+            print('Cumulative time expended:', str(time.time() - start) + ' seconds\n\n')
+            return
+        except Exception as e:
+            raise e
+
+    try:
+        run_routine()
+        print('index ' + str(outer_idx), height_file + ' DONE\n')
+    except Exception as e:
+        print(e)
+    return
+
+
 if __name__ == '__main__':
-    f_names_xml = [f for f in os.listdir(BASE_PATH_BLENDER + 'Bl_xml_files/') if os.path.isdir(f)]
+    f_names_xml = [f for f in os.listdir(BASE_PATH_BLENDER + 'Bl_xml_files/')
+                   if os.path.isdir(BASE_PATH_BLENDER + 'Bl_xml_files/' + f)]
+    print(len(f_names_xml))
     # f[0:-5] to remove the tiff
-    f_names_sig_map = [f[0:-5] for f in os.listdir(BASE_PATH_SIONNA) if os.path.isfile(f)]
+    f_names_sig_map = [f[0:-5] for f in os.listdir(BASE_PATH_SIONNA)
+                       if os.path.isfile(BASE_PATH_SIONNA + f)]
+    print(len(f_names_sig_map))
     futures = []
+    # print(f_names_sig_map)
+
+    """
+    for idx, f_name_xml in enumerate(f_names_xml):
+        if f_name_xml not in f_names_sig_map:
+            try:
+                sionna_run(f_name_xml+'.txt', EXTRA_HEIGHT, 10, BASE_PATH_BLENDER, BASE_PATH_SIONNA, idx)
+                break
+            except Exception as e:
+                print(e)
+    """
+
+    """
+        Comment out the second call to futures.append using sionna_run if you want to run xml_to_heatmap_one_run
+        as a subprocess. 
+        sionna_run duplicates the functionalities of xml_to_heatmap_one_run without having to run 
+        xml_to_heatmap_one_run as a subprocess. 
+    """
+
     with concurrent.futures.ProcessPoolExecutor(max_workers=NUM_OF_PROCESS) as executor:
         for idx, f_name_xml in enumerate(f_names_xml):
-            if f_name_xml in f_names_sig_map:
+            if f_name_xml not in f_names_sig_map:  # skip cmaps that have already been generated
+                # Running as sub-process:
+                # futures.append(executor.submit(subprocess.run,
+                #                                ['python', 'xml_to_heatmap_one_run.py',
+                #                                 '--height_file', str(f_name_xml) + '.txt',
+                #                                 '--extra_height', str(EXTRA_HEIGHT),
+                #                                 '--cm_cell_size', str(10),
+                #                                 '--BASE_PATH_BLENDER', str(BASE_PATH_BLENDER),
+                #                                 '--BASE_PATH_SIONNA', str(BASE_PATH_SIONNA),
+                #                                 '--outer_idx', str(idx)],
+                #                                capture_output=True, text=True))
+
+                # Running as a function in this file.
+                futures.append(executor.submit(sionna_run,
+                                               str(f_name_xml + '.txt'),
+                                               EXTRA_HEIGHT,
+                                               10,
+                                               BASE_PATH_BLENDER,
+                                               BASE_PATH_SIONNA,
+                                               idx))
+
+                # print('Doing:', f_name_xml, idx)
+                if len(futures) % 10 == 0:
+                    wait(futures)
                 continue
-            futures.append(executor.submit(subprocess.run,
-                                           ['python', 'xml_to_heatmap_one_run.py',
-                                            '--height_file', str(f_name_xml) + '.txt',
-                                            '--extra_height', float(EXTRA_HEIGHT),
-                                            '--cm_cell_size', float(10),
-                                            '--BASE_PATH_BLENDER', str(BASE_PATH_BLENDER),
-                                            'BASE_PATH_SIONNA', str(BASE_PATH_SIONNA)],
-                                           capture_output=True, text=True))
+            # print('Skipping:', f_name_xml, idx)
+
     wait(futures)
+    print('DONE')
