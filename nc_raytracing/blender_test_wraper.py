c3215f1b481f213279f17eea574b10187b5bd8c6
Zeyu Li <lizeyu090312@gmail.com>
2023-06-27 22:45:02 -0400

    prints what to type in the command line when running blender_test_command_line manually

--- nc_raytracing/blender_test_wraper.py
+++ nc_raytracing/blender_test_wraper.py
@@ -13,8 +13,8 @@ BLENDER_PATH = os.environ.get('BLENDER_PATH')
 BLENDER_COMMAND_LINE_PATH = os.environ.get('BLENDER_COMMAND_LINE_PATH')
 BLENDER_OSM_DOWNLOAD_PATH = os.environ.get('BLENDER_OSM_DOWNLOAD_PATH')
 START_FROM_IDX = 0
-NUM_OF_PROCESS = 8
-DECIMATE_FACTOR = 1
+NUM_OF_PROCESS = 5
+DECIMATE_FACTOR = 90
 RES_FILE_NAME = os.environ.get('RES_FILE_NAME')
 
 
@@ -39,8 +39,7 @@ if __name__ == '__main__':
     os.makedirs( BASE_PATH + 'Bl_building_npy/', exist_ok=True)
     os.makedirs( BASE_PATH + 'Bl_xml_files/', exist_ok=True)
     # f_names_xml = [f for f in os.listdir(BASE_PATH + 'Bl_xml_files/')
-    #                if os.path.isdir(BASE_PATH + 'Bl_xml_files/' + f)]
-    
+    #                 if os.path.isdir(BASE_PATH + 'Bl_xml_files/' + f) and f != '.DS_Store']
     with open(BASE_PATH + RES_FILE_NAME, 'r') as loc_fPtr:
         lines = loc_fPtr.readlines()
     futures = []
@@ -48,9 +47,21 @@ if __name__ == '__main__':
         for idx, line in enumerate(lines):
             if idx < START_FROM_IDX:
                 continue
-            # print(idx)
             # file format: minLon, maxLat, maxLon, minLat, percent, idx_uuid
             minLonOut, maxLatOut, maxLonOut, minLatOut, percent, idx_uuid = splitting_a_line(lll=line, uuid_incl='y')
+            # print(' '.join([BLENDER_PATH, "--background",
+            #      "--python",
+            #      BLENDER_COMMAND_LINE_PATH.replace(' ', '\ '), "--",
+            #      "--idx", str(idx),
+            #      "--minLon", str(minLonOut),
+            #      "--maxLat", str(maxLatOut),
+            #      "--maxLon", str(maxLonOut),
+            #      "--minLat", str(minLatOut),
+            #      "--building_to_area_ratio", str(percent),
+            #      "--decimate_factor", str(DECIMATE_FACTOR),
+            #      "--BASE_PATH", str(BASE_PATH).replace(' ', '\ '),
+            #      "--BLENDER_OSM_DOWNLOAD_PATH", str(BLENDER_OSM_DOWNLOAD_PATH).replace(' ', '\ '),
+            #      "--idx_uuid", str(idx_uuid)]))
             futures.append(executor.submit(
                 subprocess.run,
                 [BLENDER_PATH, "--background",
@@ -67,8 +78,6 @@ if __name__ == '__main__':
                  "--BLENDER_OSM_DOWNLOAD_PATH", str(BLENDER_OSM_DOWNLOAD_PATH),
                  "--idx_uuid", str(idx_uuid)],
                 capture_output=True, text=True))
-            if idx > 5:
-                break
         for idx, future in enumerate(concurrent.futures.as_completed(futures)):
             try:
                 data = future.result().stdout
