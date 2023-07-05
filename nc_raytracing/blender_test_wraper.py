f0ca7dee4c72beedc8276f02426036eaebd4be93
Zeyu Li <lizeyu090312@gmail.com>
2023-07-04 21:43:18 -0400

    added argument

--- nc_raytracing/blender_test_wraper.py
+++ nc_raytracing/blender_test_wraper.py
@@ -12,10 +12,11 @@ BASE_PATH = os.environ.get('BASE_PATH')
 BLENDER_PATH = os.environ.get('BLENDER_PATH')
 BLENDER_COMMAND_LINE_PATH = os.environ.get('BLENDER_COMMAND_LINE_PATH')
 BLENDER_OSM_DOWNLOAD_PATH = os.environ.get('BLENDER_OSM_DOWNLOAD_PATH')
-START_FROM_IDX = 102
-STOP_AT_IDX = 200
+START_FROM_IDX = 0
+STOP_AT_IDX = 10
 NUM_OF_PROCESS = 5
-DECIMATE_FACTOR = 90
+DECIMATE_FACTOR = 10
+TERRAIN_OR_PLANE = 'plane'
 RES_FILE_NAME = os.environ.get('RES_FILE_NAME')
 
 
@@ -35,10 +36,10 @@ def splitting_a_line(lll, uuid_incl='n'):
 
 if __name__ == '__main__':
     print(BASE_PATH)
-    os.makedirs( BASE_PATH + 'height_at_origin/', exist_ok=True)
-    os.makedirs( BASE_PATH + 'Bl_terrain_npy/', exist_ok=True)
-    os.makedirs( BASE_PATH + 'Bl_building_npy/', exist_ok=True)
-    os.makedirs( BASE_PATH + 'Bl_xml_files/', exist_ok=True)
+    os.makedirs(BASE_PATH + 'height_at_origin/', exist_ok=True)
+    os.makedirs(BASE_PATH + 'Bl_terrain_npy/', exist_ok=True)
+    os.makedirs(BASE_PATH + 'Bl_building_npy/', exist_ok=True)
+    os.makedirs(BASE_PATH + 'Bl_xml_files/', exist_ok=True)
     # f_names_xml = [f for f in os.listdir(BASE_PATH + 'Bl_xml_files/')
     #                 if os.path.isdir(BASE_PATH + 'Bl_xml_files/' + f) and f != '.DS_Store']
     with open(BASE_PATH + RES_FILE_NAME, 'r') as loc_fPtr:
@@ -52,19 +53,20 @@ if __name__ == '__main__':
                 break
             # file format: (minLon,maxLat,maxLon,minLat),percent,idx_uuid\n
             minLonOut, maxLatOut, maxLonOut, minLatOut, percent, idx_uuid = splitting_a_line(lll=line, uuid_incl='y')
-            # print(' '.join([BLENDER_PATH, "--background",
-            #      "--python",
-            #      BLENDER_COMMAND_LINE_PATH.replace(' ', '\ '), "--",
-            #      "--idx", str(idx),
-            #      "--minLon", str(minLonOut),
-            #      "--maxLat", str(maxLatOut),
-            #      "--maxLon", str(maxLonOut),
-            #      "--minLat", str(minLatOut),
-            #      "--building_to_area_ratio", str(percent),
-            #      "--decimate_factor", str(DECIMATE_FACTOR),
-            #      "--BASE_PATH", str(BASE_PATH).replace(' ', '\ '),
-            #      "--BLENDER_OSM_DOWNLOAD_PATH", str(BLENDER_OSM_DOWNLOAD_PATH).replace(' ', '\ '),
-            #      "--idx_uuid", str(idx_uuid)]))
+            print(' '.join([BLENDER_PATH,  # "--background",
+                            "--python",
+                            BLENDER_COMMAND_LINE_PATH.replace(' ', '\ '), "--",
+                            "--idx", str(idx),
+                            "--minLon", str(minLonOut),
+                            "--maxLat", str(maxLatOut),
+                            "--maxLon", str(maxLonOut),
+                            "--minLat", str(minLatOut),
+                            "--building_to_area_ratio", str(percent),
+                            "--decimate_factor", str(DECIMATE_FACTOR),
+                            "--BASE_PATH", str(BASE_PATH).replace(' ', '\ '),
+                            "--BLENDER_OSM_DOWNLOAD_PATH", str(BLENDER_OSM_DOWNLOAD_PATH).replace(' ', '\ '),
+                            "--idx_uuid", str(idx_uuid),
+                            '--terrain_or_plane', TERRAIN_OR_PLANE]))
             futures.append(executor.submit(
                 subprocess.run,
                 [BLENDER_PATH, "--background",
@@ -79,7 +81,8 @@ if __name__ == '__main__':
                  "--decimate_factor", str(DECIMATE_FACTOR),
                  "--BASE_PATH", str(BASE_PATH),
                  "--BLENDER_OSM_DOWNLOAD_PATH", str(BLENDER_OSM_DOWNLOAD_PATH),
-                 "--idx_uuid", str(idx_uuid)],
+                 "--idx_uuid", str(idx_uuid),
+                 '--terrain_or_plane', TERRAIN_OR_PLANE],
                 capture_output=True, text=True))
         for idx, future in enumerate(concurrent.futures.as_completed(futures)):
             try:
@@ -88,4 +91,3 @@ if __name__ == '__main__':
             except Exception as e:
                 print(e)
         wait(futures)
-
